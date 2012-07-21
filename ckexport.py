ed1ef31ae808a04e85618b0039d4b18d909ace1b
Dmitry Lazarko <alt0064@gmail.com>
2012-07-21 14:26:28 +0400

    Grouper

--- ckexport.py
+++ ckexport.py
@@ -4,6 +4,7 @@ import sqlite3
 import csv
 from datetime import datetime
 import os
+from itertools import groupby
 
 
 def dict_factory(cursor, row):
@@ -18,9 +19,9 @@ class ExporterDoesNotExist(Exception):
 
 
 class Exporter(object):
-    def export(self, data, path):
+    def export(self, data, path, **kw):
         exporter = self.get_exporter(path)
-        return exporter(data, path)
+        return exporter(data, path, **kw)
 
     def get_exporter(self, path):
         filename, file_extension = os.path.splitext(path)
@@ -30,39 +31,66 @@ class Exporter(object):
             raise ExporterDoesNotExist('Exporter {0} does not exist'.format(file_extension))
         return exporter
 
-    def export_csv(self, data, path):
+    def export_csv(self, data, path, fields=None):
         if not data:
             return
-        fields = data[0].keys()
 
         with open(path, 'wb') as f:
+            list_writer = csv.writer(f)
             # Write titles
-            writer = csv.writer(f)
-            writer.writerow(fields)
+            if fields:
+                list_writer.writerow(fields)
+            else:
+                for row in data:
+                    if isinstance(row, dict):
+                        list_writer.writerow(row.keys())
+                        break
 
-            writer = csv.DictWriter(f, fields)
+            # Write rows
             for row in data:
-                for k,v in row.items():
-                    #print type(v)
-                    if isinstance(v, unicode):
-                        row[k] = v.encode('utf8')
-                    if isinstance(v, float):
-                        row[k] = '{0}'.format(v).replace('.', ',')
-                writer.writerow(row)
+                if isinstance(row, dict):
+                    dict_writer = csv.DictWriter(f, fields or row.keys())
+                    for k,v in row.items():
+                        if isinstance(v, unicode):
+                            row[k] = v.encode('utf8')
+                        if isinstance(v, float):
+                            row[k] = '{0}'.format(v).replace('.', ',')
+                    dict_writer.writerow(row)
+                if isinstance(row, unicode):
+                    list_writer.writerow([''])
+
+
+class Grouper(object):
+    def __init__(self, data, groupby='date'):
+        self.groupby = groupby
+        self.data = data
+
+    def group(self):
+        grouper = getattr(self, 'groupby_{0}'.format(self.groupby), None)
+        return grouper(self.data)
+
+    def groupby_date(self, data):
+        grouped_data = []
+        for key, group in groupby(data, lambda x: x['Date'].split(' ')[0]):
+            grouped_data.append(key)
+            for row in group:
+                grouped_data.append(row)
+        return grouped_data
 
 
 class CoinKeeper(object):
-    def __init__(self, db_path, exporter=None):
+    def __init__(self, db_path, exporter=None, grouper=Grouper):
         assert db_path
         self.connection = sqlite3.connect(db_path)
         self.connection.row_factory = dict_factory
         self.cursor = self.connection.cursor()
+        self.grouper = grouper
         if exporter:
             self.exporter = exporter
         else:
             self.exporter = Exporter
 
-    def get_transactions(self, fields=['Name', 'Note', 'DefaultAmount', 'Icon', 'Date'], order_by='date'):
+    def get_transactions(self, fields=['*'], order_by='date'):
         fields = ','.join(fields)
         sql = """
             SELECT
@@ -79,28 +107,29 @@ class CoinKeeper(object):
         """.format(**locals())
         return list(self.cursor.execute(sql))
 
-    def export(self, data, path=None):
+    def export(self, fields=None, path=None):
+        data = self.get_transactions(fields)
+        if self.grouper:
+            grouper = self.grouper(data)
+            data = grouper.group()
+
         if not path:
             path = datetime.now().strftime('%Y-%m-%d.csv')
         exporter = self.exporter()
-        exporter.export(data, path)
+        exporter.export(data, path, fields=fields)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Coin keeper (http://coinkeeper.me/) database export tool')
     parser.add_argument('-d', '--db', type=str, help='database path', default='CoinKeeper2.db3')
-    parser.add_argument('-f', '--fields', type=str, help='fields from database to be extracted', nargs='*')
+    parser.add_argument('-f', '--fields', type=str, help='fields from database to be extracted', nargs='*', default=['Date', 'Name', 'DefaultAmount', 'Note', 'Icon'])
     parser.add_argument('-t', '--target', type=str, help='target file(s)', nargs='*', default=None)
     args = parser.parse_args()
 
     ck = CoinKeeper(args.db)
-    transaction_kw = {}
-    if args.fields:
-        transaction_kw = {'fields': args.fields}
-    data = ck.get_transactions(**transaction_kw)
 
     if args.target:
         for target in args.target:
-            ck.export(data, target)
+            ck.export(target, fields=args.fields)
     else:
-        ck.export(data)
+        ck.export(fields=args.fields)
